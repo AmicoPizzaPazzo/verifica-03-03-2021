a1366700c747bb160e0cec5c454fc00d1000baa0
AmicoPizzaPazzo <78431434+AmicoPizzaPazzo@users.noreply.github.com>
2021-03-03 09:42:38 +0100

    Add files via upload

---
+++ teoria.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class teoria {
+    /*Che cosa sono overloading e overriding?
+    overload è quando il si scrivono più metodi identificati dallo stesso nome che però ha parametri di ingresso diversi.
+    ovveride è una riscrittura di un metodo di un classe che abbiamo ereditato
+    Quali sono le caratteristiche fondamentali delle programmazione ad oggetti?
+    -Overload, Override, ereditarietà e polimorfismo
+     */
+}
